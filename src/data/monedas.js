ea94fd7ff80f3ed12657a5d77d6752c1cfbb24fa
Facundo Almaraz <facundoalmaraz8@gmail.com>
2022-11-20 05:30:00 -0300

    Version 1.0

---
+++ src/data/monedas.js
@@ -0,0 +1,17 @@
+const monedas = [
+  {
+    id: "USD",
+    nombre: "Dolar de Estados Unidos",
+  },
+
+  {
+    id: "EUR",
+    nombre: "Euro",
+  },
+  {
+    id: "ARS",
+    nombre: "Peso Argentino",
+  },
+];
+
+export { monedas };
